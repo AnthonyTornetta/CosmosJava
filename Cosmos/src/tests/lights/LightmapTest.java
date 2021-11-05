e7fe81035dd0d45c8d00738776cafd43a79c6b16
act5373 <cornchipgonecodin@gmail.com>
2021-11-05 04:21:23 -0400

    Reformatted tests

--- Cosmos/src/tests/lights/LightmapTest.java
+++ Cosmos/src/tests/lights/LightmapTest.java
@@ -14,107 +14,104 @@ class LightmapTest
 {
 	private LightMap map;
 	private LightSource src;
-	
+
 	private static final int W = 11, H = 11;
-	
+
 	@BeforeEach
 	void before()
 	{
 		map = new LightMap(W, H, 1);
 		src = new LightSource(5, 1, 0, 0);
 	}
-	
+
 	@Test
 	void test()
 	{
 		assertEquals(new Vector3f(0, 0, 0), map.lightAt(0, 0, 0));
-		
+
 		map.addLight(src, 0, 0, 0);
 		map.updateMap();
-		
-		float[][] actualMap = new float[][]
-			{
-				{ 1.0f, 0.8f, 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.8f, 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }, 
-				{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, }
-			};
-		
+
+		float[][] actualMap = new float[][] { { 1.0f, 0.8f, 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.8f, 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.6f, 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, } };
+
 		float[][] generated = new float[H][W];
-		
-		for(int y = 0; y < H; y++)
+
+		for (int y = 0; y < H; y++)
 		{
-			for(int x = 0; x < W; x++)
+			for (int x = 0; x < W; x++)
 			{
 				generated[y][x] = map.lightAt(x, y, 0).x();
 			}
 		}
-		
-		for(int y = 0; y < H; y++)
+
+		for (int y = 0; y < H; y++)
 			assertArrayEquals(actualMap[y], generated[y]);
-		
+
 		map.removeLight(0, 0, 0);
 		map.updateMap();
-		for(int z = 0; z < 1; z++)
-			for(int y = 0; y < H; y++)
-				for(int x = 0; x < W; x++)
+		for (int z = 0; z < 1; z++)
+			for (int y = 0; y < H; y++)
+				for (int x = 0; x < W; x++)
 					assertEquals(new Vector3f(0, 0, 0), map.lightAt(x, y, z));
 	}
-	
+
 	@Test
 	public void test2()
 	{
 		map.addLight(src, W / 2, H / 2, 0);
 		map.updateMap();
-		
+
 		map.addLight(src, 0, H / 2, 0);
 		map.updateMap();
-		
+
 //		map.printMap();
-		
+
 		map.removeLight(W / 2, H / 2, 0);
 		map.updateMap();
 //		map.printMap();
-		
-		map.setBlocking(1, H/2, 0);
+
+		map.setBlocking(1, H / 2, 0);
 		map.updateMap();
 //		map.printMap();
-		
+
 		map.removeBlocking(1, H / 2, 0);
 		map.updateMap();
 //		map.printMap();
 	}
-	
+
 	@Test
 	public void test3()
 	{
 		map.addLight(src, W / 2, H / 2, 0);
 		map.updateMap();
 //		map.printMap();
-		
+
 		map.addLight(src, 0, H / 2, 0);
 		map.updateMap();
 //		map.printMap();
 	}
-	
+
 	@Test
 	public void test4()
 	{
 		map.addLight(src, W / 2, H / 2, 0);
 		map.updateMap();
 		map.printMap();
-		
+
 		map.setBlocking(W / 2 - 1, H / 2, 0);
 		map.setBlocking(W / 2 + 1, H / 2, 0);
 		map.updateMap();
-		
+
 		map.printDBGCombo();
 	}
 }
--- Cosmos/src/tests/structure/TestStructure.java
+++ Cosmos/src/tests/structure/TestStructure.java
@@ -23,132 +23,132 @@ class TestStructure
 {
 	private static void fill(Structure s, Block b)
 	{
-		for(int z = 0; z < s.length(); z++)
+		for (int z = 0; z < s.length(); z++)
 		{
-			for(int y = 0; y < s.height(); y++)
+			for (int y = 0; y < s.height(); y++)
 			{
-				for(int x = 0; x < s.width(); x++)
+				for (int x = 0; x < s.width(); x++)
 				{
 					s.block(x, y, z, b);
 				}
 			}
 		}
 	}
-	
+
 	Structure a;
-	
+
 	private void init(int w, int h, int l)
 	{
 		World wd = new World();
-		
+
 		a = new Planet(wd, w, h, l, 1);
-		
+
 		fill(a, Blocks.STONE);
-		
+
 		a.addToWorld(new Transform());
 	}
 
 	@BeforeEach
 	void before()
 	{
-		
+
 	}
-	
+
 	@AfterEach
 	void after()
 	{
 		a = null;
 	}
-	
+
 	private static final float EPSILON = 1e-5f;
-	
+
 	private static void assertVectorEquals(Vector3fc expected, Vector3fc actual)
 	{
-		if(expected.x() - actual.x() > EPSILON 
-				|| expected.y() - actual.y() > EPSILON
-				|| expected.z() - actual.z() > EPSILON)
+		if (expected.x() - actual.x() > EPSILON || expected.y() - actual.y() > EPSILON
+			|| expected.z() - actual.z() > EPSILON)
 		{
 			assertEquals(expected, actual); // this will be false but give nice printout
 		}
 		else
 			assertTrue(true); // it passed
 	}
-	
+
 	@Test
 	void blocks()
 	{
 		final int W = 16, H = 16, L = 16;
-		
+
 		init(W, H, L);
-		
+
 		OBBCollider c = a.obbForBlock(a.chunk(0, 0, 0), 0, 0, 0);
-		
+
+		assertEquals(new OBBCollider(new Vector3f(0, 0, 0), new Orientation(), new Vector3f(8, 8, 8)),
+			a.obbForChunk(a.chunk(0, 0, 0)));
+
 		assertEquals(
-				new OBBCollider(new Vector3f(0, 0, 0), new Orientation(), 
-						new Vector3f(8, 8, 8)), a.obbForChunk(a.chunk(0, 0, 0)));
-		
-		assertEquals(
-				new OBBCollider(new Vector3f(-7.5f, -7.5f, -7.5f), new Orientation(), 
-						new Vector3f(0.5f, 0.5f, 0.5f)), c);
-		
-		init(W+W, H+H, L+L);
-		
-		
+			new OBBCollider(new Vector3f(-7.5f, -7.5f, -7.5f), new Orientation(), new Vector3f(0.5f, 0.5f, 0.5f)), c);
+
+		init(W + W, H + H, L + L);
+
 		c = a.obbForBlock(a.chunk(0, 0, 0), 0, 0, 0);
-		
+
 		assertEquals(
-				new OBBCollider(new Vector3f(-15.5f, -15.5f, -15.5f), new Orientation(), 
-						new Vector3f(0.5f, 0.5f, 0.5f)), c);
+			new OBBCollider(new Vector3f(-15.5f, -15.5f, -15.5f), new Orientation(), new Vector3f(0.5f, 0.5f, 0.5f)),
+			c);
 
 		a.body().transform().orientation().rotateRelative(new Vector3f(0, Maths.PI, 0));
 		a.body().transform().position(new Vector3f(100, 0, 0));
 		c = a.obbForBlock(a.chunk(0, 0, 0), 15, 0, 0);
-		
-		assertEquals(
-				new OBBCollider(new Vector3f(100.5f, -15.5f, 15.5f), a.body().transform().orientation(), 
-						new Vector3f(0.5f, 0.5f, 0.5f)), c);
+
+		assertEquals(new OBBCollider(new Vector3f(100.5f, -15.5f, 15.5f), a.body().transform().orientation(),
+			new Vector3f(0.5f, 0.5f, 0.5f)), c);
 	}
-	
+
 	@Test
 	void structureEven()
-	{	
+	{
 		init(32, 32, 32);
-		
+
 		a.body().transform().position(new Vector3f(100, 150, 200));
 
 		assertVectorEquals(new Vector3f(-8, -8, -8), a.chunk(0, 0, 0).relativePosition());
 		assertVectorEquals(new Vector3f(-8, -8, -8), a.chunkRelativePosCentered(a.chunk(0, 0, 0), new Vector3f()));
-		
-		assertVectorEquals(new Vector3f(100 - 8, 150 - 8, 200 - 8), a.chunkWorldPosCentered(a.chunk(0, 0, 0), new Vector3f()));
-		
+
+		assertVectorEquals(new Vector3f(100 - 8, 150 - 8, 200 - 8),
+			a.chunkWorldPosCentered(a.chunk(0, 0, 0), new Vector3f()));
+
 		assertVectorEquals(new Vector3f(8, 8, 8), a.chunk(1, 1, 1).relativePosition());
 		assertVectorEquals(new Vector3f(8, 8, 8), a.chunkRelativePosCentered(a.chunk(1, 1, 1), new Vector3f()));
-		
-		assertVectorEquals(new Vector3f(100 + 8, 150 + 8, 200 + 8), a.chunkWorldPosCentered(a.chunk(1, 1, 1), new Vector3f()));
+
+		assertVectorEquals(new Vector3f(100 + 8, 150 + 8, 200 + 8),
+			a.chunkWorldPosCentered(a.chunk(1, 1, 1), new Vector3f()));
 	}
-	
+
 	@Test
 	void structureOdd()
-	{	
+	{
 		init(48, 48, 48);
-		
+
 		a.body().transform().position(new Vector3f(100, 150, 200));
-		
+
 		assertVectorEquals(new Vector3f(-16, -16, -16), a.chunk(0, 0, 0).relativePosition());
 		assertVectorEquals(new Vector3f(-16, -16, -16), a.chunkRelativePosCentered(a.chunk(0, 0, 0), new Vector3f()));
-		
-		assertVectorEquals(new Vector3f(100 -16, 150 - 16, 200 - 16), a.chunkWorldPosCentered(a.chunk(0, 0, 0), new Vector3f()));
-		
+
+		assertVectorEquals(new Vector3f(100 - 16, 150 - 16, 200 - 16),
+			a.chunkWorldPosCentered(a.chunk(0, 0, 0), new Vector3f()));
+
 		assertVectorEquals(new Vector3f(16, 16, 16), a.chunk(2, 2, 2).relativePosition());
 		assertVectorEquals(new Vector3f(16, 16, 16), a.chunkRelativePosCentered(a.chunk(2, 2, 2), new Vector3f()));
-		
-		assertVectorEquals(new Vector3f(100 + 16, 150 + 16, 200 + 16), a.chunkWorldPosCentered(a.chunk(2, 2, 2), new Vector3f()));
-		
+
+		assertVectorEquals(new Vector3f(100 + 16, 150 + 16, 200 + 16),
+			a.chunkWorldPosCentered(a.chunk(2, 2, 2), new Vector3f()));
+
 		Vector3f pos = new Vector3f(a.body().transform().position());
 		a.body().transform().position(new Vector3f());
 		a.body().transform().orientation().rotateRelative(new Vector3f(0, Maths.PI, 0));
 		a.body().transform().position(pos);
-		
-		assertVectorEquals(new Vector3f(100 - 16, 150 + 16, 200 - 16), a.chunkWorldPosCentered(a.chunk(2, 2, 2), new Vector3f()));
+
+		assertVectorEquals(new Vector3f(100 - 16, 150 + 16, 200 - 16),
+			a.chunkWorldPosCentered(a.chunk(2, 2, 2), new Vector3f()));
 	}
 }
